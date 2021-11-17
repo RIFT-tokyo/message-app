8554e2b21ba6eccfb8acbddb687f3ddbf52000f6
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-17 18:01:10 +0900

    show chat history

--- server/src/message/message.gateway.ts
+++ server/src/message/message.gateway.ts
@@ -1,16 +1,21 @@
 import { Inject } from '@nestjs/common';
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { Message } from './message.entity';
 import { MessageService } from './message.service';
 
 @WebSocketGateway(4000, { namespace: 'message', cors: true })
-export class MessageGateway {
+export class MessageGateway implements OnGatewayConnection {
   @Inject()
   messageService: MessageService;
 
   @WebSocketServer()
   wss: Server;
+
+  public async handleConnection(client: Socket, ...args: any[]) {
+    const messages = await this.messageService.getAll();
+    client.emit('all-messages-to-client', messages);
+  }
 
   @SubscribeMessage('new-message-to-server')
   async handleNewMessage(@ConnectedSocket() client: Socket, @MessageBody() data: {sender: string; message: string}) {
